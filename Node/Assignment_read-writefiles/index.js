42163f970a83696c4f9a9de56cd1a482cfafd257
Rishabh Sagar <rishabh.sagar@biz2x.com>
2025-09-30 12:06:00 +0530

    v3

--- Node/Assignment_read-writefiles/index.js
+++ Node/Assignment_read-writefiles/index.js
@@ -1,1 +1,17 @@
+//This file is to be executed before writeFileSync.js
+let fs = require('fs')
+var obj = require('./readwrite')   //loading the user defined module / local module
 
+var readLine = require('readline');  // this interface is used to read the data from the console
+
+var r1 = readLine.createInterface({
+    input : process.stdin,
+    output: process.stdout
+});
+
+r1.question('Enter name', function(name){
+    r1.question('Enter price', function(price){
+        obj.writeCustomer(name,price)
+    });
+})
+console.log("Completed")
